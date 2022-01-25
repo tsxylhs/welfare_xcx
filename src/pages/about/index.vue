499c4b2acf3dd9aa043200169ebb483c18b6fa45
lvhoushuai <lvhoushuai_vendor@sensetime.com>
2022-01-25 18:31:21 +0800

    add about index.vue

--- src/pages/about/index.vue
+++ src/pages/about/index.vue
@@ -2,7 +2,7 @@
   .w-100
     nav-bar(:title="'关于小程序'" :backVisible="true" :home-path="'/pages/index/main'")
     .p-20p
-      .text-indent 该是一个为彩民随机生成幸运数。记录彩民购彩小程序
+      .text-indent 该是一个为彩民随机生成幸运数。记录彩民购彩小程序，兑奖信息。及中奖信息不做可信凭证使用。出现中奖情况请复查中奖信息。谢谢。
 </template>
 
 <script>
